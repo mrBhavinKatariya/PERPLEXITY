00ea7408b46edffe0cee9d9c4de5ea3e04a95934
mrBhavinKatariya <mr.bhavin009@gmail.com>
2025-03-19 10:52:55 +0530

    Refactor handleUserBetEndpoint: add logging for request body to assist with debugging

--- Backend/src/controllers/prediction.controllers.js
+++ Backend/src/controllers/prediction.controllers.js
@@ -320,6 +320,8 @@ const deductUserBalance = async (userId, totalAmount) => {
 const handleUserBetEndpoint = asyncHandler(async (req, res) => {
   const { userId, totalAmount, number } = req.body;
 
+  console.log("req.body",req.body);
+  
   // Initial validation
   if (!mongoose.Types.ObjectId.isValid(userId)) {
     return res.status(400).json(
