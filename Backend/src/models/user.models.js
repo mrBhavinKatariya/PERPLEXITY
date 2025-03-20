b679ea08665e564e734b9b766987b97ec0d95daf
mrBhavinKatariya <mr.bhavin009@gmail.com>
2025-03-20 16:57:21 +0530

    Simplify password hashing in user model by removing salt generation

--- Backend/src/models/user.models.js
+++ Backend/src/models/user.models.js
@@ -187,13 +187,8 @@ userSchema.methods.createPasswordResetToken = function () {
 userSchema.pre("save", async function(next) {
     if (!this.isModified("password")) return next();
     
-    try {
-        const salt = await bcrypt.genSalt(10);
-        this.password = await bcrypt.hash(this.password, salt);
-        next();
-    } catch (err) {
-        return next(err);
-    }
+    this.password = await bcrypt.hash(this.password, 10)
+    next()
 });
 
 userSchema.methods.isPasswordCorrect = async function(password) {
