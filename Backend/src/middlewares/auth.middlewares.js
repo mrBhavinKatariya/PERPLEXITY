0fa3c920c192ce5a4c641921dfe56ec60194660d
mrBhavinKatariya <mr.bhavin009@gmail.com>
2025-03-20 23:57:29 +0530

    Rename ApiError to ApiErrors for consistency in error handling middleware

--- Backend/src/middlewares/auth.middlewares.js
+++ Backend/src/middlewares/auth.middlewares.js
@@ -1,6 +1,6 @@
 
 
-import { ApiError } from "../utils/ApiError.js";
+import { ApiErrors } from "../utils/ApiErrors.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import jwt from "jsonwebtoken"
 import { User } from "../models/user.model.js";
