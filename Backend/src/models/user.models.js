a112e8b088f23225576a8bc7435a1dc18f4c3d1b
mrBhavinKatariya <mr.bhavin009@gmail.com>
2025-03-20 17:49:23 +0530

    Refactor user model methods for password handling and restore pre-save hook

--- Backend/src/models/user.models.js
+++ Backend/src/models/user.models.js
@@ -184,16 +184,16 @@ userSchema.methods.createPasswordResetToken = function () {
   };
 
 
-// userSchema.pre("save", async function(next) {
-//     if (!this.isModified("password")) return next();
+userSchema.pre("save", async function(next) {
+    if (!this.isModified("password")) return next();
     
-//     this.password = await bcrypt.hash(this.password, 10)
-//     next()
-// });
+    this.password = await bcrypt.hash(this.password, 10)
+    next()
+});
 
-// userSchema.methods.isPasswordCorrect = async function(password) {
-//     return await bcrypt.compare(password, this.password);
-// };
+userSchema.methods.isPasswordCorrect = async function(password) {
+    return await bcrypt.compare(password, this.password);
+};
 
 // Removed async from token generators (JWT is synchronous)
 userSchema.methods.generateAccessToken = function() {
