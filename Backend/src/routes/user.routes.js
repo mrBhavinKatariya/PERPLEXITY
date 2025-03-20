9f38e7c7db2334db4062834ac0330b3ed3602674
mrBhavinKatariya <mr.bhavin009@gmail.com>
2025-03-21 00:01:46 +0530

    Refactor user routes and update ResetPassword component to improve code clarity and maintainability

--- Backend/src/routes/user.routes.js
+++ Backend/src/routes/user.routes.js
@@ -3,7 +3,7 @@ import { loginUser, logOutUser, registerUser } from "../controllers/user.control
 // import { upload } from "../middlewares/multer.middlewares.js";
 import { verifyJWT, verifyJWTS } from "../middlewares/auth.middlewares.js";
 import { getCurrentUser } from "../controllers/user.controllers.js";
-import { deductUserBalance, deleteOldRandomNumbers, forgotPassword, getCountdownTimeEndpoint, getLastTenRandomNumbersEndpoint, getRandomNumberEndpoint, getUserBalanceEndpoint, getUserBetHistoryEndpoint, handleUserBetEndpoint, RazorPayCreatePaymentOrder, RazorpayPaymentAndUpdateBalance, storeUTRNumberEndpoint, updateUserBalanceEndpoint , resetPassword, ResetForForgot, changeCurrentPassword} from "../controllers/prediction.controllers.js";
+import { deductUserBalance, deleteOldRandomNumbers, getCountdownTimeEndpoint, getLastTenRandomNumbersEndpoint, getRandomNumberEndpoint, getUserBalanceEndpoint, getUserBetHistoryEndpoint, handleUserBetEndpoint, RazorPayCreatePaymentOrder, RazorpayPaymentAndUpdateBalance, storeUTRNumberEndpoint, updateUserBalanceEndpoint , changeCurrentPassword} from "../controllers/prediction.controllers.js";
 // import { generateRandomNumber, generateRandomNumberEndpoint, getCountdownTimeEndpoint, handleRandomNumberGeneration } from "../controllers/prediction.controllers.js";
 
 const router = Router();
--- Frontend/src/Component/ResetPassword.component.jsx
+++ Frontend/src/Component/ResetPassword.component.jsx
@@ -1,7 +1,8 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { motion } from 'framer-motion';
 import { useNavigate, useParams } from 'react-router-dom';
 import axios from 'axios';
+// import 
 
 const ResetnewPassword = () => {
     const { token } = useParams();
