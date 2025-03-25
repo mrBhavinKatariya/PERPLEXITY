2b44f2f9486e6870d4f79b3b56d0bfe76474fd71
mrBhavinKatariya <mr.bhavin009@gmail.com>
2025-03-26 00:43:28 +0530

    Add isActive field to GameSession model for enhanced session management

--- Backend/src/models/GameSession.models.js
+++ Backend/src/models/GameSession.models.js
@@ -6,7 +6,9 @@ const gameSessionSchema = new mongoose.Schema({
   color: String,
   startTime: { type: Date, default: Date.now },
   endTime: { type: Date, required: true },
-  status: { type: String, enum: ['active', 'completed'], default: 'active' }
+  status: { type: String, enum: ['active', 'completed'], default: 'active' },
+  isActive: { type: Boolean, default: true }
+
 });
 
 gameSessionSchema.index({ isActive: 1 }, { unique: true, partialFilterExpression: { isActive: true } });
