8bb70475c0e02b403437a8751dbc83b00b4fbf05
mrBhavinKatariya <mr.bhavin009@gmail.com>
2025-03-26 13:06:32 +0530

    Remove unused imports and commented-out routes in user.routes.js for improved clarity

--- Backend/src/routes/user.routes.js
+++ Backend/src/routes/user.routes.js
@@ -3,7 +3,7 @@ import { loginUser, logOutUser, registerUser } from "../controllers/user.control
 // import { upload } from "../middlewares/multer.middlewares.js";
 import { verifyJWT, verifyJWTS } from "../middlewares/auth.middlewares.js";
 import { getCurrentUser } from "../controllers/user.controllers.js";
-import { deductUserBalance, deleteOldRandomNumbers, getCountdownTimeEndpoint, getLastTenRandomNumbersEndpoint, getRandomNumberEndpoint, getUserBalanceEndpoint, getUserBetHistoryEndpoint, handleUserBetEndpoint, RazorPayCreatePaymentOrder, RazorpayPaymentAndUpdateBalance, storeUTRNumberEndpoint, updateUserBalanceEndpoint , changeCurrentPassword, createFundAccount, initiateWithdrawal, handlePayoutWebhook, transactionHistory, getReferralEarnings, handleUserSession, handleUserBet} from "../controllers/prediction.controllers.js";
+import { deductUserBalance, deleteOldRandomNumbers, getCountdownTimeEndpoint, getLastTenRandomNumbersEndpoint, getRandomNumberEndpoint, getUserBalanceEndpoint, getUserBetHistoryEndpoint, handleUserBetEndpoint, RazorPayCreatePaymentOrder, RazorpayPaymentAndUpdateBalance, storeUTRNumberEndpoint, updateUserBalanceEndpoint , changeCurrentPassword, createFundAccount, initiateWithdrawal, handlePayoutWebhook, transactionHistory, getReferralEarnings} from "../controllers/prediction.controllers.js";
 // import { generateRandomNumber, generateRandomNumberEndpoint, getCountdownTimeEndpoint, handleRandomNumberGeneration } from "../controllers/prediction.controllers.js";
 
 const router = Router();
@@ -32,10 +32,6 @@ router.route("/transactions-history/:userid").get(verifyJWT,transactionHistory)
 router.route('/referral/earnings').get(getReferralEarnings);
 
 
-// --------------new------------------///
-
-router.route("/session").get(handleUserSession);
-router.route("/bet").post(handleUserBet);
 
 
 
