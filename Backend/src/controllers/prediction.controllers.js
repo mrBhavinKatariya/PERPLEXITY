913609b67246f40134fca2e18a22d778512d77a6
mrBhavinKatariya <mr.bhavin009@gmail.com>
2025-03-28 19:53:39 +0530

    Update multiplier value in winning logic for consistency

--- Backend/src/controllers/prediction.controllers.js
+++ Backend/src/controllers/prediction.controllers.js
@@ -487,7 +487,7 @@ const handleUserBetEndpoint = asyncHandler(async (req, res) => {
     // Winning logic में निम्नलिखित बदलाव
 if (typeof number === 'number') {
   if (randomNumber === number) {
-    multiplier =  9; // विशेष केस
+    multiplier =  6; 
     result = "WIN";
   }
 } else {
