7e627f12602ebe88363d75055b384ebe8dbe756a
mrBhavinKatariya <mr.bhavin009@gmail.com>
2025-03-31 17:09:03 +0530

    Comment out admin-related routes and middleware in user.routes.js

--- Backend/src/routes/user.routes.js
+++ Backend/src/routes/user.routes.js
@@ -1,11 +1,9 @@
 import { Router } from "express";
-import { loginUser, logOutUser, registerUser,getCurrentUser, registerAdmin,
-  loginAdmin,
-  setNextColor,
-  getOverrideStatus
+import { loginUser, logOutUser, registerUser,getCurrentUser
+  
  } from "../controllers/user.controllers.js";
 // import { upload } from "../middlewares/multer.middlewares.js";
-import { verifyAdmin, verifyJWT, verifyJWTS } from "../middlewares/auth.middlewares.js";
+import {  verifyJWT, verifyJWTS } from "../middlewares/auth.middlewares.js";
 
 // import { getCurrentUser } from "../controllers/user.controllers.js";
 import { deductUserBalance, deleteOldRandomNumbers, getCountdownTimeEndpoint, getLastTenRandomNumbersEndpoint, getRandomNumberEndpoint, getUserBalanceEndpoint, getUserBetHistoryEndpoint, handleUserBetEndpoint, RazorPayCreatePaymentOrder, RazorpayPaymentAndUpdateBalance, storeUTRNumberEndpoint, updateUserBalanceEndpoint , changeCurrentPassword, createFundAccount, initiateWithdrawal, handlePayoutWebhook, transactionHistory, getReferralEarnings} from "../controllers/prediction.controllers.js";
@@ -13,18 +11,18 @@ import { AdminOverride } from "../models/AdminOverrideColor.js";
 // import { generateRandomNumber, generateRandomNumberEndpoint, getCountdownTimeEndpoint, handleRandomNumberGeneration } from "../controllers/prediction.controllers.js";
 
 
-const checkAdmin = (req, res, next) => {
-    if(req.user.role !== "admin") throw new Error("Unauthorized");
-    next();
-  };
+// const checkAdmin = (req, res, next) => {
+//     if(req.user.role !== "admin") throw new Error("Unauthorized");
+//     next();
+//   };
 
 const router = Router();
 
 router.route("/register").post(registerUser);
 router.route("/login").post(loginUser); // Removed verifyJWT middleware
 // router.route("/logout").post(verifyJWT, logOutUser);
-router.route("/admin/register").post(registerAdmin);
-router.route("/admin/login").post(loginAdmin);
+// router.route("/admin/register").post(registerAdmin);
+// router.route("/admin/login").post(loginAdmin);
 router.route("/current-user").get(verifyJWT, getCurrentUser);
 // router.route("/randomeNumber").get(handleRandomNumberGeneration)
 router.route("/randomenumber").get(getRandomNumberEndpoint);
@@ -48,9 +46,9 @@ router.route('/referral/earnings').get(getReferralEarnings);
 // ----------- Admin routes ---------------
 
 // router.use(verifyAdmin);
-router.route("/admin/set-color").post(setNextColor);
-router.route("/admin/override-status").get(getOverrideStatus);
-// router.route("/current-admin").get(verifyAdmin, getCurrentUser);
+// router.route("/admin/set-color").post(setNextColor);
+// router.route("/admin/override-status").get(getOverrideStatus);
+// // router.route("/current-admin").get(verifyAdmin, getCurrentUser);
 
 
 
