d29c8411086507120c145dcc37d476c493b55d05
mrBhavinKatariya <mr.bhavin009@gmail.com>
2025-03-31 17:11:53 +0530

    Comment out admin-related routes and middleware in user.routes.js

--- Backend/src/routes/user.routes.js
+++ Backend/src/routes/user.routes.js
@@ -5,24 +5,17 @@ import { loginUser, logOutUser, registerUser,getCurrentUser
 // import { upload } from "../middlewares/multer.middlewares.js";
 import {  verifyJWT, verifyJWTS } from "../middlewares/auth.middlewares.js";
 
-// import { getCurrentUser } from "../controllers/user.controllers.js";
 import { deductUserBalance, deleteOldRandomNumbers, getCountdownTimeEndpoint, getLastTenRandomNumbersEndpoint, getRandomNumberEndpoint, getUserBalanceEndpoint, getUserBetHistoryEndpoint, handleUserBetEndpoint, RazorPayCreatePaymentOrder, RazorpayPaymentAndUpdateBalance, storeUTRNumberEndpoint, updateUserBalanceEndpoint , changeCurrentPassword, createFundAccount, initiateWithdrawal, handlePayoutWebhook, transactionHistory, getReferralEarnings} from "../controllers/prediction.controllers.js";
 import { AdminOverride } from "../models/AdminOverrideColor.js";
 // import { generateRandomNumber, generateRandomNumberEndpoint, getCountdownTimeEndpoint, handleRandomNumberGeneration } from "../controllers/prediction.controllers.js";
 
 
-// const checkAdmin = (req, res, next) => {
-//     if(req.user.role !== "admin") throw new Error("Unauthorized");
-//     next();
-//   };
-
 const router = Router();
 
 router.route("/register").post(registerUser);
 router.route("/login").post(loginUser); // Removed verifyJWT middleware
-// router.route("/logout").post(verifyJWT, logOutUser);
-// router.route("/admin/register").post(registerAdmin);
-// router.route("/admin/login").post(loginAdmin);
+router.route("/logout").post(verifyJWT, logOutUser);
+
 router.route("/current-user").get(verifyJWT, getCurrentUser);
 // router.route("/randomeNumber").get(handleRandomNumberGeneration)
 router.route("/randomenumber").get(getRandomNumberEndpoint);
@@ -42,14 +35,6 @@ router.route("/fund-account").post(createFundAccount);
 router.route("/withdraw").post(initiateWithdrawal);
 router.route("/transactions-history/:userid").get(verifyJWT,transactionHistory)
 router.route('/referral/earnings').get(getReferralEarnings);
-
-// ----------- Admin routes ---------------
-
-// router.use(verifyAdmin);
-// router.route("/admin/set-color").post(setNextColor);
-// router.route("/admin/override-status").get(getOverrideStatus);
-// // router.route("/current-admin").get(verifyAdmin, getCurrentUser);
-
 
 
 
