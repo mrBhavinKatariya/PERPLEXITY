3b52b89b0d213c6822c9c9f3873e7c86db3c083c
mrBhavinKatariya <mr.bhavin009@gmail.com>
2025-04-04 17:10:06 +0530

    Update createFundAccount to store and return last 4 digits of account number

--- Backend/src/controllers/prediction.controllers.js
+++ Backend/src/controllers/prediction.controllers.js
@@ -1174,7 +1174,7 @@ const createFundAccount = asyncHandler(async (req, res) => {
       $push: {
         bankAccounts: {
           fundAccountId: fundAccount.id,
-          accountNumber: accountNumber,
+          last4: accountNumber,
           bankName: "Bank Name", // You can use IFSC to get actual bank name
           contactId: contact.id,
           ifsc: ifscCode
@@ -1185,7 +1185,7 @@ const createFundAccount = asyncHandler(async (req, res) => {
     res.status(200).json({
       success: true,
       fundAccountId: fundAccount.id,
-      accountNumber: accountNumber.id,
+      last4: accountNumber,
       contactId: contact.id
     });
 
