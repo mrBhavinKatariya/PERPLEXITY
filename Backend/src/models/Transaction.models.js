f1a766a1772bc86e9f8110349fcf72ef635e4c75
mrBhavinKatariya <mr.bhavin009@gmail.com>
2025-04-06 14:02:45 +0530

    Refactor transaction model to change type enum from "deposit" and "withdrawal" to "credit" and "debit"; update required fields for accountNumber and UPIId

--- Backend/src/models/Transaction.models.js
+++ Backend/src/models/Transaction.models.js
@@ -14,7 +14,7 @@ const transactionSchema = new mongoose.Schema(
     },
     type: {
       type: String,
-      enum: ["deposit", "withdrawal"],
+      enum: ["credit", "debit"],
       required: true,
     },
     paymentMethod: {
@@ -33,15 +33,15 @@ const transactionSchema = new mongoose.Schema(
     },
     accountNumber: {
       type: String,
-      required: true,
+      // required: true,
     },
     UPIId: {
       type: String,
-      required: true,
+      // required: true,
     },
     ifsc: {
       type: String,
-      required: true,
+      // required: true,
       validate: {
         validator: (v) => /^[A-Za-z]{4}0[A-Z0-9]{6}$/.test(v),
         message: "Invalid IFSC format",
@@ -53,3 +53,59 @@ const transactionSchema = new mongoose.Schema(
 );
 
 export const Transaction = mongoose.model("Transaction", transactionSchema);
+
+
+// import mongoose from "mongoose";
+
+// const transactionSchema = new mongoose.Schema(
+//   {
+//     userId: {
+//       type: mongoose.Schema.Types.ObjectId,
+//       ref: "User",
+//       required: true,
+//     },
+//     amount: {
+//       type: Number,
+//       required: true,
+//     },
+//     type: {
+//       type: String,
+//       enum: ["deposit", "withdrawal"],
+//       required: true,
+//     },
+//     paymentMethod: {
+//       type: String,
+//       required: true,
+//     },
+   
+//     transactionId: {
+//       type: String,
+//       required: true,
+//     },
+//     status: {
+//       type: String,
+//       enum: ["pending","processing", "completed", "failed"],
+//       default: "pending"
+//     },
+//     accountNumber: {
+//       type: String,
+//       // required: true,
+//     },
+//     UPIId: {
+//       type: String,
+//       // required: true,
+//     },
+//     ifsc: {
+//       type: String,
+//       // required: true,
+//       validate: {
+//         validator: (v) => /^[A-Za-z]{4}0[A-Z0-9]{6}$/.test(v),
+//         message: "Invalid IFSC format",
+//       },
+//     },
+
+//   },
+//   { timestamps: true }
+// );
+
+// export const Transaction = mongoose.model("Transaction", transactionSchema);
