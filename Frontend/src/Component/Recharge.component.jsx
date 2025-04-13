ee9b4731b826ab46a023adbea461d46fcb8abc7d
mrBhavinKatariya <mr.bhavin009@gmail.com>
2025-04-13 14:35:01 +0530

    Fix minimum amount validation in handleProceedToPayment function

--- Frontend/src/Component/Recharge.component.jsx
+++ Frontend/src/Component/Recharge.component.jsx
@@ -83,7 +83,7 @@ const RechargePage = ({ user, onClose }) => {
   };
 
   const handleProceedToPayment = () => {
-    if (!amount || amount < 100) {
+    if (!amount || amount < 0) {
       setError("Minimum amount is 100");
       return;
     }
