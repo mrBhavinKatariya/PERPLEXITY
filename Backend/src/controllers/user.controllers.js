8c07f181704f6c7b7ab2bd4496e721d79858ad40
mrBhavinKatariya <mr.bhavin009@gmail.com>
2025-04-15 12:12:51 +0530

    Fix typo in payment status from 'completed' to 'panding' in createPayment function

--- Backend/src/controllers/user.controllers.js
+++ Backend/src/controllers/user.controllers.js
@@ -108,7 +108,7 @@ const createPayment = asyncHandler(async (req, res) => {
                 ifsc: BANK_DETAILS.ifsc,
                 upiId: BANK_DETAILS.upiId
             },
-            status: 'completed'
+            status: 'panding'
         }, "Payment created successfully")
     );
 });
