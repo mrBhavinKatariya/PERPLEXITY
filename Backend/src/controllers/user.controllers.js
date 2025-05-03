0d5e0a7493581389ff2e9b36adad493ef1e44c32
mrBhavinKatariya <mr.bhavin009@gmail.com>
2025-05-03 19:51:40 +0530

    Add logging for payment verification process

--- Backend/src/controllers/user.controllers.js
+++ Backend/src/controllers/user.controllers.js
@@ -156,6 +156,8 @@ const verifyPayment = asyncHandler(async (req, res) => {
         { new: true }
     );
 
+    console.log("payment",payment);
+    
     const user = await User.findById(userId);
     if (!user) {
         throw new ApiErrors(404, "User not found");
